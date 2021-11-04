d9e4acd1ad6b56a8aed496dfaeb75be2c9bd7e61
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-04 10:19:54 +0800

    second commit~

--- main.cc
+++ main.cc
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <stdlib.h>
+#include <functional>
 #include "test.h"
+
+typedef std::uint64_t uint64;
+
 int main()
 {
     std::cout << "Hello Geek\n";
+    std::function<uint64()>* p;
     helloWorld();
     return 0;
 }
