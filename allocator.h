439cf87ebf493a54c70e4ac6ced4af0433fdaf44
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-10 16:24:47 +0800

    test static_cast<int>.

--- allocator.h
+++ allocator.h
@@ -151,14 +151,72 @@ struct AllocatorAttributes {
     bool nic_compatible() const { return value & (0x1 << 1); }
     void set_gpu_compatible(bool v) { value |= (static_cast<int>(v) << 2); }
     bool gpu_compatible() const { return value & (0x1 << 2); }
+    void Merge(AllocatorAttributes other){
+        value |= other.value;
+        if (scope_id != other.scope_id){
+            CHECK(scope_id == 0 || other.scope_id == 0)
+                <<"At least one scope_id should be zero to merge "
+                  "AllocatorAttributes but found this.scope_id="
+                << scope_id << " and other.scope_id=" << other.scope_id;
+            scope_id = scope_id == 0 ? other.scope_id : scope_id;
+        }
+    }
+
+    bool IsEqualOrLessRestrictiveThan(const AllocatorAttributes& other) const{
+        return (value | other.value) == other.value;
+    }
 
     uint32 value = 0;
-    int32 scoped_id = 0;
+    int32 scope_id = 0;
     std::string DebugString() const;
 
 };
 
+Allocator* cpu_allocator_base();
+
+Allocator* cpu_allocator(int numa_node = 0);
+
+// Enables AllocatorStats in the default CPU allocator implementation.  By
+// default, it's disabled.
+void EnableCPUAllocatorStats();
+// Disables AllocatorStats in the default CPU allocator implementation.  By
+// default, it's disabled.
+void DisableCPUAllocatorStats();
+bool CPUAllocatorStatsEnabled();
+
+// Enables full statistics collection in the default CPU allocator
+// implementation.  By default, it's disabled.
+void EnableCPUAllocatorFullStats();
+bool CPUAllocatorFullStatsEnabled();
+
+// An object that does the underlying suballoc/free of memory for a higher-level
+// allocator.  The expectation is that the higher-level allocator is doing some
+// kind of cache or pool management so that it will call SubAllocator::Alloc and
+// Free relatively infrequently, compared to the number of times its own
+// AllocateRaw and Free methods are called.
+class SubAllocator {
+    public:
+        typedef std::function<void(void*, int index, size_t)> Visitor;
+
+        SubAllocator(const std::vector<Visitor>& alloc_visitors,
+                     const std::vector<Visitor>& free_visitors);
+        
+        virtual ~SubAllocator() {}
+
+        virtual void* Alloc(size_t alignment, size_t num_bytes,
+                            size_t* bytes_received) = 0;
+        virtual void Free(void* ptr, size_t num_bytes) = 0;
+
+        virtual bool SupportCoalescing() const = 0;
+
+    protected:
+        void VisitAlloc(void* ptr, int index, size_t num_bytes);
+        void VisitFree(void* ptr, int index, size_t num_bytes);
+
+        const std::vector<Visitor> alloc_visitors_;
+        const std::vector<Visitor> free_visitors_;
+};
+
+
 }
-
-
 #endif
