45e5b44fa1f5786c084237f6f02ff582fad4bd10
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-10 18:27:57 +0800

    test allocatorStats.

--- main.cc
+++ main.cc
@@ -13,7 +13,7 @@ int main()
     std::function<tfcore::uint64()>* p;
 
     tfcore::AllocationAttributes(false,false,p);
-    tfcore::AllocatorStats as = tfcore::AllocatorStats();
+    const tfcore::AllocatorStats as = tfcore::AllocatorStats();
     std::string s = as.DebugString();
     
 
