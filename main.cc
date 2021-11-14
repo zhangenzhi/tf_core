6a4ba3c6fa2ee0f2c9907e725d9f3b19a5729155
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-14 19:53:20 +0800

    test cmake.

--- main.cc
+++ main.cc
@@ -17,8 +17,8 @@ int main()
     // std::string s = as->DebugString();
     
 
-    tfcore::Allocator();
-    std::cout << PREDICT_FALSE(10)<<std::endl;
+    tfcore::Allocator a();
+    std::cout << a.PREDICT_FALSE(10)<<std::endl;
     tfcore::helloWorld();
     tfcore::TestClass* t;
     t->HelloConst();
