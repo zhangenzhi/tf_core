15662843deb1a9796827bcae9d59f57abb91fc5d
zhangenzhi <zhangsuiyu657@gmail.com>
2021-11-14 20:13:42 +0800

    test cmake.

--- stringprintf.cc
+++ stringprintf.cc
@@ -2,10 +2,63 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdarg.h>
+
 #include "stringprintf.h"
 
 namespace tfcore{
 namespace strings{
+
+void Appendv(std::string* dst, const char* format, va_list ap) {
+  // First try with a small fixed size buffer
+  static const int kSpaceLength = 1024;
+  char space[kSpaceLength];
+
+  // It's possible for methods that use a va_list to invalidate
+  // the data in it upon use.  The fix is to make a copy
+  // of the structure before using it and use that copy instead.
+  va_list backup_ap;
+  va_copy(backup_ap, ap);
+  int result = vsnprintf(space, kSpaceLength, format, backup_ap);
+  va_end(backup_ap);
+
+  if (result < kSpaceLength) {
+    if (result >= 0) {
+      // Normal case -- everything fit.
+      dst->append(space, result);
+      return;
+    }
+
+#ifdef _MSC_VER
+      // Error or MSVC running out of space.  MSVC 8.0 and higher
+      // can be asked about space needed with the special idiom below:
+      va_copy(backup_ap, ap);
+      result = vsnprintf(nullptr, 0, format, backup_ap);
+      va_end(backup_ap);
+#endif
+
+    if (result < 0) {
+      // Just an error.
+      return;
+    }
+  }
+
+  // Increase the buffer size to the size requested by vsnprintf,
+  // plus one for the closing \0.
+  int length = result + 1;
+  char* buf = new char[length];
+
+  // Restore the va_list before we use it again
+  va_copy(backup_ap, ap);
+  result = vsnprintf(buf, length, format, backup_ap);
+  va_end(backup_ap);
+
+  if (result >= 0 && result < length) {
+    // It fit
+    dst->append(buf, result);
+  }
+  delete[] buf;
+}
+
 
 std::string Printf(const char* format, ...) {
   va_list ap;
@@ -14,7 +67,14 @@ std::string Printf(const char* format, ...) {
   Appendv(&result, format, ap);
   va_end(ap);
   return result;
-};
+}
+
+void Appendf(std::string* dst, const char* format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  Appendv(dst, format, ap);
+  va_end(ap);
+}
 
 }
 }
